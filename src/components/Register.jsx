63e25401fffb3296efd1d5eba583aa2a3e8a37fc
Mmsshakil <softhandshakil@gmail.com>
2023-10-04 15:21:21 +0600

    registration page done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,44 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        console.log(e);
+    }
+
     return (
-        <div>
-            <h2>register</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl p-5 bg-base-100">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name="email" type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name="password" type="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    <div className="text-center">
+                        <p>Already have account? Please<Link to="/login"><button className="btn btn-link">Login</button></Link> </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
