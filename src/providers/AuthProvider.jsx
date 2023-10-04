e1b293eea31ce576b00ee28f684929b123c4a2d2
Mmsshakil <softhandshakil@gmail.com>
2023-10-04 17:28:34 +0600

    auth provider and firebase setup for registration but i don't understand this

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,13 +1,22 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import PropTypes from 'prop-types';
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebase/firebase.config";
 
 // 1. create createContext
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
 
-    const authInfo = { name: 'hello world' }
+    const [user , setUser] = useState(null);
+
+    const creatUser = (email, password) =>{
+
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = { user, creatUser }
+    console.log(authInfo);
 
     return (
         // 2. set provider with value
